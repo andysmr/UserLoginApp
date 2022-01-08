6510841451056aea185ee8b832a8abc951613231
andySmr <andrey180696007@yandex.ru>
2022-01-08 19:15:02 +0400

    start

--- UserLoginApp/ViewController.swift
+++ UserLoginApp/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var password: UITextField!
+    @IBOutlet var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loginButton.layer.cornerRadius = 8
     }
 
 
